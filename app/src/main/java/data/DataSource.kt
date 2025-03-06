0312087d7505116329eb87bb47709848af5f1768
AbrahamLD <mario.lopez462@hotmail.com>
2025-03-05 22:22:40 -0600

    se realizó el Ejercicio 3: Manejo de Card del profesor, cambiando el diseño del menu, agregando precio y oferta.

--- app/src/main/java/data/DataSource.kt
+++ app/src/main/java/data/DataSource.kt
@@ -6,12 +6,12 @@ import model.Platillo
 class DataSource {
     fun LoadPlatillos(): List<Platillo>{
         return listOf<Platillo>(
-            Platillo(R.string.desayuno, R.drawable.desayuno),
-            Platillo(R.string.hamburgesa, R.drawable.hamburgesa),
-            Platillo(R.string.pizza, R.drawable.pizza),
-            Platillo(R.string.postre, R.drawable.postre),
-            Platillo(R.string.pozole, R.drawable.pozole),
-            Platillo(R.string.tacos, R.drawable.tacos),
+            Platillo(R.string.desayuno, R.drawable.desayuno, R.string.desayuno_precio, R.string.desayuno_oferta),
+            Platillo(R.string.hamburgesa, R.drawable.hamburgesa, R.string.hamburgesa_precio, R.string.hamburgesa_oferta),
+            Platillo(R.string.pizza, R.drawable.pizza, R.string.pizza_precio, R.string.pizza_oferta),
+            Platillo(R.string.postre, R.drawable.postre,R.string.postre_precio, R.string.postre_oferta),
+            Platillo(R.string.pozole, R.drawable.pozole, R.string.pozole_precio, R.string.pozole_oferta),
+            Platillo(R.string.tacos, R.drawable.tacos, R.string.tacos_precio, R.string.tacos_oferta),
         )
     }
 }
--- app/src/main/java/model/Platillo.kt
+++ app/src/main/java/model/Platillo.kt
@@ -6,4 +6,6 @@ import androidx.annotation.StringRes
 data class Platillo(
     @StringRes val stringResourceId:Int,
     @DrawableRes val drawableResId: Int,
+    @StringRes val stringResourcePrecio: Int,
+    @StringRes val stringResourceOferta: Int,
     )
